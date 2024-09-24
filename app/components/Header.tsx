888b824988ad266621838a11b9f142e14e82ff57
Mohd Sahil <mohdsahilsiddiqui36@gmail.com>
2024-09-24 23:48:39 +0530

    svg modules and header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import Image from 'next/image';
+import Logo from '../public/Logo.svg';
 import { ModeToggle } from './ui/mode-toggle';
 
 export default function Header() {
     return (
         <div className='flex items-center justify-between p-4'>
-            <div>
-                {/* <Image src='/Logo.PNG' alt='logo' width={16} height={25} className='h-10 w-auto'/> */}LOGO
+            <div className='flex items-center'>
+                {/* Add conditional text color for light and dark modes */}
+                {/* <Logo className='h-8 w-auto fill-black dark:fill-white' /> */}
+                <span className='text-xl font-bold text-black dark:text-white'>PlanIt</span>
             </div>
             <div>
-                <ModeToggle/>
+                <ModeToggle />
             </div>
         </div>
     );
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+const withSvgr = require('@svgr/webpack');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
