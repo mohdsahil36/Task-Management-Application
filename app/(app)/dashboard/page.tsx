c8e1dec3b1a9a2fcb57ee26d8b536713aad5f83c
Mohd Sahil <mohdsahilsiddiqui36@gmail.com>
2024-10-06 23:13:10 +0530

    sidebar ui in-progress

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -2,18 +2,21 @@ import React from 'react';
 import { auth, currentUser } from '@clerk/nextjs/server';
 import { Toaster } from 'sonner';
 import SignUpPage from '@/app/(auth)/sign-up/[[...sign-up]]/page';
+import Sidebar from '@/components/Sidebar';
 
-
-export default async function Dashboard(){
-    const userId=auth();
-    const user=await currentUser();
-    if(!userId || !user){
-        return <SignUpPage/>
+export default async function Dashboard() {
+    const userId = auth();
+    const user = await currentUser();
+    if (!userId || !user) {
+        return <SignUpPage />;
     }
-    console.log(user);
-    return(
-        <div className='h-full'>
-            <h1 className='text-xl font-bold text-slate-200'>This is the dashboard</h1>
+
+    return (
+        <div className='h-screen relative'>
+            <Sidebar />
+            <div className='bg-neutral-200 w-full h-full'>
+                Data content
+            </div>
         </div>
-    )
+    );
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { IconMenu2 } from '@tabler/icons-react';
 
-export default function Sidebar(){
+export default function Sidebar() {
   return (
-    <aside>
-      This will be the sidebar
+    <aside className='fixed top-0 left-0 h-screen w-28 md:w-52 bg-gray-800 text-white z-50 p-2.5'>
+      <div className='flex justify-end'>
+        <IconMenu2 stroke={1} size={20} className='md:size-25' />
+      </div>
     </aside>
-  )
+  );
 }
