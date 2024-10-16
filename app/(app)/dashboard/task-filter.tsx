1fc9d31570faeebfe803db33880efeb4ed876acf
Mohd Sahil <mohdsahilsiddiqui36@gmail.com>
2024-10-16 23:01:00 +0530

    debugging

--- app/(app)/dashboard/task-filter.tsx
+++ app/(app)/dashboard/task-filter.tsx
@@ -7,8 +7,9 @@ export default function TaskFilter() {
     return (
         <>
             <div className="flex w-full max-w-sm items-center space-x-2">
-                <Input type="email" placeholder="Search your task" />
+                <Input type='text' placeholder='Search Task'/>
                 <Button type="submit">Search</Button>
+
             </div>
             <div className='text-start'>
                 <Button className='mt-3 md:mt-0'>Add Task <Plus className="h-4 w-4 ml-2"/></Button>
