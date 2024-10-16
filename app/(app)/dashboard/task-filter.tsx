f001b141472a58b1655986adbaf90ed24b2c0e09
Mohd Sahil <mohdsahilsiddiqui36@gmail.com>
2024-10-16 23:06:07 +0530

    debugging

--- app/(app)/dashboard/task-filter.tsx
+++ app/(app)/dashboard/task-filter.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { Plus } from 'lucide-react';
+import { Input } from '@/components/ui/input';
 
 export default function TaskFilter() {
     return (
         <>
             <div className="flex w-full max-w-sm items-center space-x-2">
-                <Input type='text' placeholder='Search Task'/>
+                <Input placeholder='Search Task' type='text'/>
                 <Button type="submit">Search</Button>
-
             </div>
             <div className='text-start'>
                 <Button className='mt-3 md:mt-0'>Add Task <Plus className="h-4 w-4 ml-2"/></Button>
