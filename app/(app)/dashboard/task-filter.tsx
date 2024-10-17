97cac1ae1aaf97b3a369b5a685949bdf72b87bf0
Mohd Sahil <mohdsahilsiddiqui36@gmail.com>
2024-10-18 00:18:52 +0530

    add task modal completed

--- app/(app)/dashboard/task-filter.tsx
+++ app/(app)/dashboard/task-filter.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from 'react';
 import { Plus } from 'lucide-react';
 import { Button } from "@/components/ui/button"
@@ -20,9 +22,20 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select"
+import { addDays, format } from "date-fns"
+import { Calendar as CalendarIcon } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+import { Calendar } from "@/components/ui/calendar"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
 
 
 export default function TaskFilter() {
+    const [date, setDate] = React.useState<Date>()
     return (
         <>
             <div className="flex w-full max-w-sm items-center space-x-2">
@@ -38,10 +51,10 @@ export default function TaskFilter() {
                         <DialogHeader>
                             <DialogTitle>Add new task</DialogTitle>
                             <DialogDescription>
-                                Add details of you task and you're good to go!
+                                Add details of you task and you&apos;re good to go!
                             </DialogDescription>
                         </DialogHeader>
-                        <div className="grid gap-4 py-4">
+                        <div className="grid gap-4">
                             <div className="">
                                 <Label htmlFor="name" className="text-right">
                                     Title
@@ -56,9 +69,9 @@ export default function TaskFilter() {
                                 <Label htmlFor="description" className="text-right">
                                     Description
                                 </Label>
-                                <Textarea placeholder="Enter your task details" className='mt-2'/>
+                                <Textarea placeholder="Enter your task details" className='mt-2' />
                             </div>
-                            <div className='flex items-center justify-start'>
+                            <div className='flex items-center justify-between'>
                                 <Label htmlFor="status" className="text-right">
                                     Status
                                 </Label>
@@ -75,6 +88,62 @@ export default function TaskFilter() {
                                         </SelectContent>
                                     </Select>
                                 </div>
+                            </div>
+                            <div className='flex items-center justify-between'>
+                                <Label htmlFor="status" className="text-right">
+                                    Priority
+                                </Label>
+                                <div className='ms-2'>
+                                    <Select>
+                                        <SelectTrigger className="w-[180px]">
+                                            <SelectValue placeholder='Select priority' />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectItem value="high">High</SelectItem>
+                                            <SelectItem value="medium">Medium</SelectItem>
+                                            <SelectItem value="low">Low</SelectItem>
+                                        </SelectContent>
+                                    </Select>
+                                </div>
+                            </div>
+                            <div className='flex items-center justify-between'>
+                                <Label htmlFor="status" className="text-right">
+                                    Deadline
+                                </Label>
+                                <Popover>
+                                    <PopoverTrigger asChild>
+                                        <Button
+                                            variant={"outline"}
+                                            className={cn(
+                                                "w-[180px] justify-start text-left font-normal",
+                                                !date && "text-muted-foreground"
+                                            )}
+                                        >
+                                            <CalendarIcon className="mr-2 h-4 w-4" />
+                                            {date ? format(date, "PPP") : <span>Pick a date</span>}
+                                        </Button>
+                                    </PopoverTrigger>
+                                    <PopoverContent className="flex w-auto flex-col space-y-2 p-2">
+                                        <Select
+                                            onValueChange={(value) =>
+                                                setDate(addDays(new Date(), parseInt(value)))
+                                            }
+                                        >
+                                            <SelectTrigger>
+                                                <SelectValue placeholder="Select" />
+                                            </SelectTrigger>
+                                            <SelectContent position="popper">
+                                                <SelectItem value="0">Today</SelectItem>
+                                                <SelectItem value="1">Tomorrow</SelectItem>
+                                                <SelectItem value="3">In 3 days</SelectItem>
+                                                <SelectItem value="7">In a week</SelectItem>
+                                            </SelectContent>
+                                        </Select>
+                                        <div className="rounded-md border">
+                                            <Calendar mode="single" selected={date} onSelect={setDate} />
+                                        </div>
+                                    </PopoverContent>
+                                </Popover>
                             </div>
                         </div>
                         <DialogFooter>
