23ee788cb368106b2405c0f29d0b83dce2b604a3
Mohd Sahil <mohdsahilsiddiqui36@gmail.com>
2024-10-19 17:22:57 +0530

    add task update

--- app/(app)/dashboard/task-filter.tsx
+++ app/(app)/dashboard/task-filter.tsx
@@ -22,7 +22,7 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select"
-import { addDays, format } from "date-fns"
+import {format } from "date-fns"
 import { Calendar as CalendarIcon } from "lucide-react"
 
 import { cn } from "@/lib/utils"
@@ -123,25 +123,13 @@ export default function TaskFilter() {
                                             {date ? format(date, "PPP") : <span>Pick a date</span>}
                                         </Button>
                                     </PopoverTrigger>
-                                    <PopoverContent className="flex w-auto flex-col space-y-2 p-2">
-                                        <Select
-                                            onValueChange={(value) =>
-                                                setDate(addDays(new Date(), parseInt(value)))
-                                            }
-                                        >
-                                            <SelectTrigger>
-                                                <SelectValue placeholder="Select" />
-                                            </SelectTrigger>
-                                            <SelectContent position="popper">
-                                                <SelectItem value="0">Today</SelectItem>
-                                                <SelectItem value="1">Tomorrow</SelectItem>
-                                                <SelectItem value="3">In 3 days</SelectItem>
-                                                <SelectItem value="7">In a week</SelectItem>
-                                            </SelectContent>
-                                        </Select>
-                                        <div className="rounded-md border">
-                                            <Calendar mode="single" selected={date} onSelect={setDate} />
-                                        </div>
+                                    <PopoverContent className="w-auto p-0">
+                                        <Calendar
+                                            mode="single"
+                                            selected={date}
+                                            onSelect={setDate}
+                                            initialFocus
+                                        />
                                     </PopoverContent>
                                 </Popover>
                             </div>
