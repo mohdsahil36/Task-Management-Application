e693278bc2a75e8d6fc7a25160c51a18eb8a2586
Mohd Sahil <mohdsahilsiddiqui36@gmail.com>
2024-10-20 23:12:02 +0530

    kanban board under build

--- app/(app)/dashboard/(kanban-board)/page.tsx
+++ app/(app)/dashboard/(kanban-board)/page.tsx
@@ -1,31 +1,37 @@
 import React from 'react';
 
 interface Columns {
+    id: number; 
     title: string;
 }
 
 const ColumnData: Columns[] = [
     {
+        id: 1,
         title: "To-Do",
     },
     {
+        id: 2, 
         title: "Under Review",
     },
     {
+        id: 3, 
         title: "In Progress",
     },
     {
+        id: 4, 
         title: "Done",
     }
 ];
 
-export default function TaskBoard(){ 
-    
-    return(
-        ColumnData.map((item,index)=>(
-            <div className='bg-black dark:bg-white text-white dark:text-black p-3 rounded-lg' key={index}>
-                <p className='text-xs md:text-lg flex items-center justify-center'>{item.title}</p>
-            </div>
-        ))
-    )
+export default function TaskBoard() {
+    return (
+        <div>
+            {ColumnData.map((item) => (
+                <div key={item.id} className='bg-black dark:bg-white text-white dark:text-black p-3 rounded-lg'>
+                    <p className='text-xs md:text-lg flex items-center justify-center'>{item.title}</p>
+                </div>
+            ))}
+        </div>
+    );
 }
