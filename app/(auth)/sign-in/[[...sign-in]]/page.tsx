7e6782cb37989f094fc3761f647cde1c08c1276d
Mohd Sahil <mohdsahilsiddiqui36@gmail.com>
2024-11-06 23:11:46 +0530

    sign up and sign in page revamp

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -6,7 +6,7 @@ const SignInPage = () => {
       <div className="md:w-6/12 bg-black dark:bg-white text-white dark:text-black flex items-center p-8 md:p-12 order-1 md:order-2 sticky top-0">
         <div className="space-y-4">
           <h1 className="text-xl md:text-4xl font-bold">
-            Welcome back! Let’s dive back into your tasks and get things done
+            Welcome back! Let&apos;s dive back into your tasks and get things done
           </h1>
           <h2 className="text-md md:text-xl mt-4 font-light">
             Log in to pick up right where you left off and stay on top of your day
