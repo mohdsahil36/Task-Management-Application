2a0d0f9b03f7fbfd35e505b0d101a8c86571ad1f
Mohd Sahil <mohdsahilsiddiqui36@gmail.com>
2024-11-06 23:09:34 +0530

    sign up and sign in page revamp

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,8 +2,20 @@ import { SignIn } from '@clerk/nextjs';
 
 const SignInPage = () => {
   return (
-    <div className="h-full flex items-center justify-center">
-      <SignIn />
+    <div className="h-screen flex flex-col md:flex-row">
+      <div className="md:w-6/12 bg-black dark:bg-white text-white dark:text-black flex items-center p-8 md:p-12 order-1 md:order-2 sticky top-0">
+        <div className="space-y-4">
+          <h1 className="text-xl md:text-4xl font-bold">
+            Welcome back! Let’s dive back into your tasks and get things done
+          </h1>
+          <h2 className="text-md md:text-xl mt-4 font-light">
+            Log in to pick up right where you left off and stay on top of your day
+          </h2>
+        </div>
+      </div>
+      <div className="md:w-6/12 flex items-center justify-center p-4 md:p-8 order-2 md:order-1">
+        <SignIn />
+      </div>
     </div>
   );
 };
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,8 +2,20 @@ import { SignUp } from '@clerk/nextjs';
 
 const SignUpPage = () => {
   return (
-    <div className="h-full flex items-center justify-center">
-      <SignUp />
+    <div className="h-screen flex flex-col md:flex-row">
+      <div className="md:w-6/12 bg-black dark:bg-white text-white dark:text-black flex items-center p-8 md:p-12 order-1 md:order-2 sticky top-0">
+        <div className="space-y-4">
+          <h1 className="text-xl md:text-4xl font-bold">
+            Hop in and let’s get stuff done! Sign up to keep your tasks in check, the easy way.
+          </h1>
+          <h2 className="text-md md:text-xl mt-4 font-light">
+            No more missed tasks or endless to-do lists – we’ll keep things simple and stress-free.
+          </h2>
+        </div>
+      </div>
+      <div className="md:w-6/12 flex items-center justify-center p-4 md:p-8 order-2 md:order-1">
+        <SignUp />
+      </div>
     </div>
   );
 };
